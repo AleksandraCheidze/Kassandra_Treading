d30e80b4ccf6adb7901a6811b463eeecdbf71575
Aleksandra Cheidze <a.cxeidze@gmail.com>
2024-08-15 21:24:16 +0200

    Первый коммит

---
+++ src/main/java/com/kassandra/TreadingApplication.java
@@ -0,0 +1,13 @@
+package com.kassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreadingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kassandra/TreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
